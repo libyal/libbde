feadc82287dc54105fa37f360af6c6a28e788c3a
Joachim Metz <joachim.metz@gmail.com>
2011-10-20 19:57:37 +0200

    Changes of version: 20111020

--- libbde/libbde_recovery.h
+++ libbde/libbde_recovery.h
@@ -32,29 +32,6 @@
 extern "C" {
 #endif
 
-typedef struct libbde_recovery_key_data libbde_recovery_key_data_t;
-
-struct libbde_recovery_key_data
-{
-	/* The last calculated SHA256 hash
-	 * This value contains the SHA256 hash calculated in the previous key iteration
-	 */
-	uint8_t last_sha256_hash[ LIBHMAC_SHA256_HASH_SIZE ];
-
-	/* The initial calculated SHA256 hash
-	 * This value contains the SHA256 hash calculated from the recovery password
-	 */
-	uint8_t initial_sha256_hash[ LIBHMAC_SHA256_HASH_SIZE ];
-
-	/* The salt
-	 */
-	uint8_t salt[ 16 ];
-
-	/* The key iteration count
-	 */
-	uint64_t iteration_count;
-};
-
 int libbde_utf8_recovery_password_calculate_hash(
      const uint8_t *utf8_string,
      size_t utf8_string_length,
@@ -67,15 +44,6 @@ int libbde_utf16_recovery_password_calculate_hash(
      size_t utf16_string_length,
      uint8_t *recovery_password_hash,
      size_t recovery_password_hash_size,
-     liberror_error_t **error );
-
-int libbde_recovery_calculate_key(
-     const uint8_t *recovery_password_hash,
-     size_t recovery_password_hash_size,
-     const uint8_t *salt,
-     size_t salt_size,
-     uint8_t *key,
-     size_t key_size,
      liberror_error_t **error );
 
 #if defined( __cplusplus )
