dab0adc05224ee713731a6c4c42fbeb58b0b7d2f
Joachim Metz <joachim.metz@gmail.com>
2011-11-20 08:55:20 +0100

    Changes of version: 20111118

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libbde package
-#
-0 4	* * *	root	[ -x /usr/bin/libbde_maintenance ] && /usr/bin/libbde_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libbde):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libbde" command="/usr/bin/libbde"
