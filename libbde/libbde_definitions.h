9911c1011920255acfa18e3fcf4afdcb50d6ef8c
Joachim Metz <joachim.metz@gmail.com>
2014-04-07 08:11:04 +0200

    Changes of version: 20140405

--- libbde/libbde_definitions.h
+++ libbde/libbde_definitions.h
@@ -34,11 +34,11 @@
  * for local use of libbde
  */
 #else
-#define LIBBDE_VERSION					20140404
+#define LIBBDE_VERSION					20140405
 
 /* The version string
  */
-#define LIBBDE_VERSION_STRING				"20140404"
+#define LIBBDE_VERSION_STRING				"20140405"
 
 /* The file access
  * bit 1        set to 1 for read access
