7ddbc03e08811cdea3f621279186a3425df21878
Joachim Metz <joachim.metz@gmail.com>
2014-10-13 08:21:39 +0200

    Worked on utility scripts

--- synclibs.sh
+++ synclibs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Script that synchronizes the local library dependencies
 #
-# Version: 20140915
+# Version: 20141004
 
 GIT_URL_PREFIX="https://github.com/libyal";
 LOCAL_LIBS="libbfio libcaes libcdata libcerror libcfile libclocale libcnotify libcpath libcsplit libcthreads libfcache libfdata libfdatetime libfguid libfvalue libhmac libuna";
@@ -13,7 +13,7 @@ for LOCAL_LIB in ${LOCAL_LIBS};
 do
 	git clone ${GIT_URL_PREFIX}/${LOCAL_LIB}.git ${LOCAL_LIB}-$$;
 
-	if [ -d ${LOCAL_LIB}-$$ ];
+	if test -d ${LOCAL_LIB}-$$;
 	then
 		LOCAL_LIB_UPPER=`echo "${LOCAL_LIB}" | tr "[a-z]" "[A-Z]"`;
 		LOCAL_LIB_VERSION=`grep -A 2 AC_INIT ${LOCAL_LIB}-$$/configure.ac | tail -n 1 | sed 's/^\s*\[\([0-9]*\)\],\s*$/\1/'`;
@@ -21,13 +21,15 @@ do
 		rm -rf ${LOCAL_LIB};
 		mkdir ${LOCAL_LIB};
 
-		if [ -d ${LOCAL_LIB} ];
+		if test -d ${LOCAL_LIB};
 		then
 			cp ${LOCAL_LIB}-$$/${LOCAL_LIB}/*.[ch] ${LOCAL_LIB};
 			cp ${LOCAL_LIB}-$$/${LOCAL_LIB}/Makefile.am ${LOCAL_LIB}/Makefile.am;
 
-SED_SCRIPT="
-1i if HAVE_LOCAL_${LOCAL_LIB_UPPER}
+SED_SCRIPT="/AM_CPPFLAGS = / {
+	i\\
+if HAVE_LOCAL_${LOCAL_LIB_UPPER}
+}
 
 /lib_LTLIBRARIES/ {
 	s/lib_LTLIBRARIES/noinst_LTLIBRARIES/
@@ -39,22 +41,22 @@ SED_SCRIPT="
 
 /${LOCAL_LIB}_la_LIBADD/ {
 :loop1
-        /${LOCAL_LIB}_la_LDFLAGS/ {
-                N
-		i endif
-                d
-        }
-        /${LOCAL_LIB}_la_LDFLAGS/ !{
-                N
-                b loop1
-        }
+	/${LOCAL_LIB}_la_LDFLAGS/ {
+		N
+		i\\
+endif
+		d
+	}
+	/${LOCAL_LIB}_la_LDFLAGS/ !{
+		N
+		b loop1
+	}
 }
 
-/EXTRA_DIST = / {
+/${LOCAL_LIB}_la_LDFLAGS/ {
 	N
-	N
-	N
-	N
+	i\\
+endif
 	d
 }
 
@@ -63,29 +65,36 @@ SED_SCRIPT="
 	N
 	d
 }";
-			sed "${SED_SCRIPT}" -i ${LOCAL_LIB}/Makefile.am;
+			echo "${SED_SCRIPT}" >> ${LOCAL_LIB}-$$.sed;
+			sed -i'~' -f ${LOCAL_LIB}-$$.sed ${LOCAL_LIB}/Makefile.am;
+			rm -f ${LOCAL_LIB}-$$.sed;
 
-SED_SCRIPT="
-/^$/ {
-        x
-        N
-        /endif$/ {
-                a \
+			sed -i'~' "/${LOCAL_LIB}_definitions.h.in/d" ${LOCAL_LIB}/Makefile.am;
+			sed -i'~' "/${LOCAL_LIB}.rc/d" ${LOCAL_LIB}/Makefile.am;
+			sed -i'~' '/EXTRA_DIST = /d' ${LOCAL_LIB}/Makefile.am;
 
-                D
-        }
+SED_SCRIPT="/^$/ {
+	x
+	N
+	/endif$/ {
+		a\\
+
+		D
+	}
 }";
-			sed "${SED_SCRIPT}" -i ${LOCAL_LIB}/Makefile.am;
+			echo "${SED_SCRIPT}" >> ${LOCAL_LIB}-$$.sed;
+			sed -i'~' -f ${LOCAL_LIB}-$$.sed ${LOCAL_LIB}/Makefile.am;
+			rm -f ${LOCAL_LIB}-$$.sed;
 
-			if [ ${LOCAL_LIB} = "libfvalue" ];
+			if test ${LOCAL_LIB} = "libfvalue";
 			then
-				sed "/@LIBFWNT_CPPFLAGS@/d" -i ${LOCAL_LIB}/Makefile.am;
+				sed -i'~' '/@LIBFWNT_CPPFLAGS@/d' ${LOCAL_LIB}/Makefile.am;
 			fi
 
 			rm -f ${LOCAL_LIB}/${LOCAL_LIB}.c;
 
 			cp ${LOCAL_LIB}-$$/${LOCAL_LIB}/${LOCAL_LIB}_definitions.h.in ${LOCAL_LIB}/${LOCAL_LIB}_definitions.h;
-			sed 's/@VERSION@/${LOCAL_LIB_VERSION}/' -i ${LOCAL_LIB}/${LOCAL_LIB}_definitions.h;
+			sed -i'~' 's/@VERSION@/${LOCAL_LIB_VERSION}/' ${LOCAL_LIB}/${LOCAL_LIB}_definitions.h;
 		fi
 		rm -rf ${LOCAL_LIB}-$$;
 	fi
