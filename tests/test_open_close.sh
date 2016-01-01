e417697f5b2339518523a1faa9b0e517a6debaf3
Joachim Metz <joachim.metz@gmail.com>
2016-01-01 15:42:45 +0100

    Applied updates and changes for deployment.

--- tests/test_open_close.sh
+++ tests/test_open_close.sh
@@ -2,7 +2,7 @@
 #
 # Library open close testing script
 #
-# Copyright (C) 2011-2015, Joachim Metz <joachim.metz@gmail.com>
+# Copyright (C) 2011-2016, Joachim Metz <joachim.metz@gmail.com>
 #
 # Refer to AUTHORS for acknowledgements.
 #
