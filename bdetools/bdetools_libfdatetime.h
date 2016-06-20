94293bc2e904e87941c9ac80437cda1f6ff5828e
Joachim Metz <joachim.metz@gmail.com>
2016-06-20 07:15:08 +0200

    Code clean up.

--- bdetools/bdetools_libfdatetime.h
+++ bdetools/bdetools_libfdatetime.h
@@ -44,7 +44,7 @@
 /* If libtool DLL support is enabled set LIBFDATETIME_DLL_IMPORT
  * before including libfdatetime.h
  */
-#if defined( _WIN32 ) && defined( DLL_IMPORT )
+#if defined( _WIN32 ) && defined( DLL_IMPORT ) && !defined( HAVE_STATIC_EXECUTABLES )
 #define LIBFDATETIME_DLL_IMPORT
 #endif
 
