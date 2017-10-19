733f3987f44a7a6b9da4d8d87d78faf2d2dda50a
Joachim Metz <joachim.metz@gmail.com>
2017-10-19 12:30:34 +0200

    Applied updates and worked on tests

--- libbde/libbde_debug.h
+++ libbde/libbde_debug.h
@@ -50,7 +50,7 @@ int libbde_debug_print_read_offsets(
      libbfio_handle_t *file_io_handle,
      libcerror_error_t **error );
 
-#endif
+#endif /* defined( HAVE_DEBUG_OUTPUT ) */
 
 #if defined( __cplusplus )
 }
