762182967434a86096d41fa93db8372841006d2d
Joachim Metz <joachim.metz@gmail.com>
2018-01-19 07:55:46 +0100

    Applied updates

--- libbde/bde_volume.h
+++ libbde/bde_volume.h
@@ -1,7 +1,7 @@
 /*
  * The BitLocker disk encryption (BDE) volume definition
  *
- * Copyright (C) 2011-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2011-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
