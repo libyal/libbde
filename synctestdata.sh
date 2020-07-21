a3c04c85129bc811026c679affe22590924fc152
Joachim Metz <joachim.metz@gmail.com>
2020-07-21 12:20:28 +0200

    Added script to set up test data for CI tests

---
+++ synctestdata.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# Script that synchronizes the local test data
+#
+# Version: 20161009
+
+TEST_SET="public";
+TEST_INPUT_DIRECTORY="tests/input";
+TEST_FILES="bdetogo.raw";
+
+mkdir -p "${TEST_INPUT_DIRECTORY}/${TEST_SET}";
+mkdir -p "${TEST_INPUT_DIRECTORY}/.libbde/${TEST_SET}";
+mkdir -p "${TEST_INPUT_DIRECTORY}/.pybde/${TEST_SET}";
+mkdir -p "${TEST_INPUT_DIRECTORY}/.bdeinfo/${TEST_SET}";
+
+for TEST_FILE in ${TEST_FILES};
+do
+	URL="https://github.com/log2timeline/dfvfs/blob/master/test_data/${TEST_FILE}?raw=true";
+
+	curl -L -o "${TEST_INPUT_DIRECTORY}/${TEST_SET}/${TEST_FILE}" ${URL};
+
+	echo "-pbde-TEST" > "${TEST_INPUT_DIRECTORY}/.libbde/${TEST_SET}/${TEST_FILE}.password";
+	echo "-pbde-TEST" > "${TEST_INPUT_DIRECTORY}/.pybde/${TEST_SET}/${TEST_FILE}.password";
+	echo "-pbde-TEST" > "${TEST_INPUT_DIRECTORY}/.bdeinfo/${TEST_SET}/${TEST_FILE}.password";
+done
+
