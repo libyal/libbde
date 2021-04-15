bc4ed2e14c2009c12163d39be0ca33b9a5632314
Joachim Metz <joachim.metz@gmail.com>
2021-04-15 19:42:43 +0200

    Applied updates

--- tests/bde_test_tools_output.c
+++ tests/bde_test_tools_output.c
@@ -39,7 +39,7 @@
 
 #include "../bdetools/bdetools_output.h"
 
-/* Tests the bdetools_output_initialize and function
+/* Tests the bdetools_output_initialize function
  * Returns 1 if successful or 0 if not
  */
 int bde_test_tools_output_initialize(
